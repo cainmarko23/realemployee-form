202fabff4a4e03252c54a5dbc98f20f363075c29
cainmarko23 <159276727+cainmarko23@users.noreply.github.com>
2024-02-18 09:53:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,12 +17,11 @@ class App extends Component {
       userPass: "",
       isUser: false
     };
-    this.addEmployee = this.addEmployee.bind(this);
-    this.saveData = this.saveData.bind(this);
+    this.add = this.add.bind(this);
     this.cancel = this.cancel.bind(this);
     this.updateTodo = this.updateTodo.bind(this);
+    this.saveData = this.saveData.bind(this);
   }
-
   /* Functional functions */
 
   signIn() {
@@ -41,18 +40,18 @@ class App extends Component {
     swal("Info!", "You are logged out succesfully", "info");
   }
 
-  addEmployee() {
+  add() {
     const { fName, lName, email, salary, date, employees } = this.state;
 
-    let employeeObj = {
+    let employeObj = {
       fName,
       lName,
       email,
       salary,
       date
     };
-    employees.push(employeeObj);
-    this.saveData(); // Save to local storage
+    employees.push(employeObj);
+    this.saveData();
     this.setState({
       employees,
       fName: "",
@@ -63,7 +62,7 @@ class App extends Component {
     });
     console.log(this.state.employees);
   }
-
+  
   updateTodo() {
     const {
       currentIndex,
@@ -82,7 +81,6 @@ class App extends Component {
       date
     };
     employees[currentIndex] = updatedObj;
-    this.saveData(); // Save to local storage
     this.setState({
       currentIndex: null,
       fName: "",
@@ -109,7 +107,6 @@ class App extends Component {
   delete(index) {
     const { employees } = this.state;
     employees.splice(index, 1);
-    this.saveData(); // Save to local storage
     this.setState({ employees, currentIndex: null });
   }
 
@@ -123,25 +120,6 @@ class App extends Component {
       date: ""
     });
   }
-
-  /* Local Storage functions */
-
-  saveData() {
-    const { employees } = this.state;
-    localStorage.setItem("employees", JSON.stringify(employees));
-  }
-
-  loadData() {
-    const storedData = localStorage.getItem("employees");
-    return storedData ? JSON.parse(storedData) : [];
-  }
-
-  componentDidMount() {
-    const employees = this.loadData();
-    this.setState({ employees });
-  }
-
-  /* Body functions */
 
   renderTodos() {
     const { employees } = this.state;
@@ -178,6 +156,13 @@ class App extends Component {
       </tbody>
     );
   }
+  saveData() {
+    // Retrieve the current employees array from the state
+    const { employees } = this.state;
+
+    // Save the employees array to local storage
+    localStorage.setItem("employees", JSON.stringify(employees));
+  }
 
   renderAddModal() {
     return (
@@ -187,4 +172,349 @@ class App extends Component {
         tabIndex="-1"
         role="dialog"
         aria-labelledby="registermodalLabel"
-        aria
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="registermodalLabel">
+                {this.state.currentIndex != null && (
+                  <p>
+                    You are editing item # {this.state.currentIndex + 1}{" "}
+                    currently
+                  </p>
+                )}
+              </h5>
+
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <label htmlFor="fName" className="">
+                First Name:
+              </label>
+              <input
+                type="text"
+                required="required"
+                className="form-control"
+                placeholder="Ali"
+                onChange={e => {
+                  this.setState({ fName: e.target.value });
+                }}
+                value={this.state.fName}
+              />
+              <label htmlFor="lName" className="">
+                Last Name:{" "}
+              </label>
+              <input
+                type="text"
+                placeholder="Khan"
+                className="form-control"
+                onChange={e => {
+                  this.setState({ lName: e.target.value });
+                }}
+                value={this.state.lName}
+              />
+              <label htmlFor="email" className="">
+                Email:
+              </label>
+              <input
+                type="email"
+                required="required"
+                placeholder="abc@example.com"
+                className="form-control"
+                onChange={e => {
+                  this.setState({ email: e.target.value });
+                }}
+                value={this.state.email}
+              />
+              <label htmlFor="salary" className="">
+                Salary:
+              </label>
+              <input
+                type="number"
+                required="required"
+                placeholder="25000"
+                className="form-control"
+                onChange={e => {
+                  this.setState({ salary: e.target.value });
+                }}
+                value={this.state.salary}
+              />
+              <label htmlFor="email" className="">
+                Job Start Date:
+              </label>
+              <input
+                type="date"
+                required="required"
+                placeholder="select date"
+                className="form-control"
+                onChange={e => {
+                  this.setState({ date: e.target.value });
+                }}
+                value={this.state.date}
+              />
+            </div>
+            <div className="modal-footer">
+              {this.state.currentIndex != null ? (
+                <span>
+                  <button
+                    onClick={this.cancel}
+                    data-dismiss="modal"
+                    className="btn btn-danger"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={this.updateTodo}
+                    data-dismiss="modal"
+                    className="btn btn-success"
+                  >
+                    Update
+                  </button>
+                </span>
+              ) : (
+                <span>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    data-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-success"
+                    data-dismiss="modal"
+                    onClick={this.add}
+                  >
+                    Create
+                  </button>
+                </span>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderAuthModal() {
+    return (
+      <div
+        className="modal fade"
+        id="signinmodal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="signinmodalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="signinmodalLabel">
+                Sign In
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                required="required"
+                placeholder="abc@example.com"
+                className="form-control"
+                onChange={e => {
+                  this.setState({ userEmail: e.target.value });
+                }}
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                required="required"
+                placeholder="**********"
+                className="form-control"
+                onChange={e => {
+                  this.setState({ userPass: e.target.value });
+                }}
+              />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-success"
+                data-dismiss="modal"
+                onClick={() => {
+                  this.signIn();
+                }}
+              >
+                Sign In
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  /* Body functions */
+
+  renderHeader() {
+    return (
+      <header>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light navBG">
+            <a className="navbar-brand" href="JavaScript:Void(0);">
+              The World's Strongest Employees List
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mr-auto" />
+              {/* <!-- Modal activate buttons --> */}
+              {!this.state.isUser ? (
+                <button
+                  className="btn btn-info my-2 my-sm-0"
+                  type="submit"
+                  data-toggle="modal"
+                  data-target="#signinmodal"
+                >
+                  <i className=" fa fa-sign-in" /> Log In
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  onClick={() => {
+                    this.logout();
+                  }}
+                >
+                  Logout
+                </button>
+              )}
+            </div>
+          </nav>
+        </div>
+        {this.renderAuthModal()}
+        {/* Add Modal */}
+        {this.renderAddModal()}
+        {/* Add Modal End*/}
+      </header>
+    );
+  }
+
+  renderBody() {
+    const { currentIndex } = this.state;
+
+    return (
+      <div style={{ marginBottom: "50px" }}>
+        <div className="container">
+          <table className="table table-bordered table-hover">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Salary</th>
+                <th scope="col">Job Start Date</th>
+                <th scope="col">Edit/Update</th>
+              </tr>
+            </thead>
+            {this.renderTodos()}
+          </table>
+        </div>
+        {currentIndex == null ? (
+          <button
+            className="btn btn-danger btn-lg"
+            style={{
+              position: "absolute",
+              top: "85%",
+              left: "95%",
+              borderRadius: "50px"
+            }}
+            data-toggle="modal"
+            data-target="#registermodal"
+          >
+            <i className="fa fa-plus" />
+          </button>
+        ) : (
+          <span />
+        )}
+      </div>
+    );
+  }
+
+  renderFooter() {
+    const footer_style = {
+      position: "fixed",
+      bottom: 0,
+      width: "100%",
+      backgroundColor: "lightgreen",
+      color: "black",
+      padding: "10px 0",
+      margin: 0
+    };
+    return (
+      <footer style={footer_style}>
+        All rights reserved | Gregory A. Martin &copy;
+      </footer>
+    );
+  }
+
+  render() {
+    console.log(this.state.isUser);
+
+    return (
+      <div className="App">
+        {!this.state.isUser ? (
+          <span>
+            {this.renderHeader()}
+            {this.renderFooter()}
+          </span>
+        ) : (
+          <span>
+            {this.renderHeader()}
+            {this.renderBody()}
+            {this.renderFooter()}
+          </span>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
